62c497ec76d91a6067a2c55718b28ec78376f230
Cecília Lima Coelho <cececilia121613@gmail.com>
2024-10-12 14:29:23 -0300

    se um dia funcionar...

--- src/pages/Train.tsx
+++ src/pages/Train.tsx
@@ -1,15 +1,37 @@
+import '../assets/all.css';
 import { useNavigate } from 'react-router-dom';
 
-function Train(){
-        const navigate = useNavigate();
+function Main() {
+    const navigate = useNavigate();
 
-        const handleClusterClick = () => {
-          navigate('/PredictionModel'); // Redireciona para a página de login
-        };
-        
-    return(
-        <button onClick={handleClusterClick}></button>
-    )
+    const handleClusterClick = () => {
+        navigate('/PredictionModel'); 
+    };
+
+    return (
+    <div className='main-container'>
+        <div className='main-card'>
+            <div className='main-card-top'>
+                <button className='main-button' onClick={handleClusterClick}>
+                    RUN
+                </button>
+                <span className='input-label'>
+                    INPUT TAGS SEPARATED BY COMMAS
+                </span>
+                <input className='input-field' placeholder='Enter XML tags'></input>
+                <button className='upload-button'>
+                    UPLOAD THE XML
+                </button>
+            </div>
+            <div className='main-card-bottom'>
+                <span className='output-label'>OUTPUT</span>
+                <div className='output-box'>
+                    <span className='output-placeholder'>nothing here yet</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
 }
 
-export default Train
+export default Main;
